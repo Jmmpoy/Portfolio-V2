cb153fdc3e58e0b0f191aed8f8513798ae2d536a
Jmmpoy <dunker35@hotmail.fr>
2022-09-06 16:34:56 +0200

    dynamic routes navigation

--- components/content.js
+++ components/content.js
@@ -12,25 +12,41 @@ export default function content() {
 
   return (
     <Container extraClasses="Content-Container   py-12   lg:py-32">
-      <p class="text-[12px] text-gray font-founders ">Selected projects</p>
+      <p class="text-[16px] text-gray font-foundersLight ">Selected projects</p>
 
-      <div class=" h-full ">
+      <div class="h-full mt-4">
         {data.map((project, index) => {
+          console.log(project);
           return (
             <div
               key={`project-${project.id}`}
-              class={`  border-t-[0.2px] border-gray  h-24 md:h-32 flex flex-col justify-center text-sm uppercase relative text-black hover:text-gray  transition duration-700`}>
+              class={` project border-t-[0.2px] border-gray  h-24 md:h-32 flex justify-between items-center text-sm uppercase relative text-black hover:text-gray  transition duration-700`}>
               <Link
-                href={{
-                  pathname: ROUTE_PROJECT_ID,
-                  query: project,
-                }}
-                as={`/projects/${project.name}`}>
+                href={`/projects/[id]`}
+                as={`/projects/${project.id}`}
+                passHref>
                 <a
-                  className={`  text-5xl xsm:text-6xl sm:text-7xl md:text-8xl font-founders tracking-tight hover:text-${project.color}  `}>
+                  className={`  text-5xl xsm:text-6xl sm:text-7xl md:text-8xl font-foundersLight tracking-tight hover:text-${project.color}  `}>
                   {project.name}
                 </a>
               </Link>
+              <div className="arrow">
+                <Link href={`/projects/[id]`} as={`/projects/${project.name}`}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="w-10 h-10">
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+                    />
+                  </svg>
+                </Link>
+              </div>
             </div>
           );
         })}
--- pages/projects/[id].js
+++ pages/projects/[id].js
@@ -1,57 +1,78 @@
 import React from "react";
 import Link from "next/link";
 import Head from "next/head";
+import Image from "next/image";
 import { useRouter } from "next/router";
 import Container from "../../components/container";
-// import data from "../../data";
+import data from "../../api.js";
 
-export default function Project() {
-  // const router = useRouter();
-  // console.log(router.query);
-  // console.log(props);
-
-  // const project = data.filter((project) => project.name === router.query.name);
-  // console.log(project);
+export default function Project({ project }) {
+  console.log("project", project);
+  
 
   return (
-    <Container extraClasses="Content-Container  h-screen py-12 overflow-auto  md:overflow-hidden  ">
-      <div className="grid  h-full mt-16  md:grid-cols-2 md:mt-32 ">
-        <div className=" w-3/5 mx-auto md:w-4/5 md:min-w-[200px] md:m-0">
+    <Container extraClasses="Content-Container relative  h-screen py-12 overflow-auto  md:overflow-hidden  ">
+      <div className="  grid  h-full mt-12  md:grid-cols-2 md:mt-32 ">
+        <div className="text-sm font-founders  w-5/5 sm:w-4/5 md:m-0 ">
+          <p className="text-5xl xsm:text-5xl sm:text-4xl md:text-4xl font-foundersLight tracking-tight hover:text-${project.color} ">
+            {project.name}
+          </p>
+          <p className="text-sm font-foundersLight mt-8 w-5/5 max-w-[450px]">
+            {project.description}
+          </p>
+          <p className="text-[12px] font-foundersLight mt-4 ">{project.role}</p>
+          <p className="text-[10px] font-foundersLight mt-[-12px]">
+            {project.year}
+          </p>
+        </div>
+        <div className="  mt-20 w-5/5  md:mt-0  md:min-w-[200px] ">
           {project && (
-            <img
-              src={project[0].url.src}
-              loading="lazy"
-              alt="Photo by Martin Sanchez"
-              class=" w-full h-auto object-cover object-center aspect-auto"
+            <Image
+              src={project.url}
+              blurDataURL={project.blurDataURL}
+              alt={project.name}
+              width={460}
+              height={258}
+              class=" min-w-[320px] w-full h-auto object-cover object-center aspect-auto"
             />
           )}
         </div>
-        <div className="text-sm font-founders  w-4/5 mx-auto md:m-0 ">
-          <p className="text-sm font-foundersLight">{project[0].description}</p>
-          <p className="text-[12px] font-foundersLight mt-8 ">
-            {project[0].name}
-          </p>
-          <p className="text-[12px] font-foundersLight mt-[-12px] ">
-            {project[0].role}
-          </p>
-          <p className="text-[10px] font-foundersLight mt-[-12px]">
-            {project[0].year}
-          </p>
-        </div>
+        {/* <Link href="/">
+          <a className="self-center mt-16 pb-4 w-6 md:mt-0 p-0">
+            <svg
+              class="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M7 16l-4-4m0 0l4-4m-4 4h18"></path>
+            </svg>
+          </a>
+        </Link> */}
       </div>
     </Container>
   );
 }
 
-export async function getStaticProps() {
+export async function getStaticProps({ params }) {
+  console.log("params", params);
+  const projects = data.filter((p) => p.id.toString() === params.id);
   return {
-    props: {
-      lists: [
-        { dirId: "1", name: "Directory 1" },
-        { dirId: "2", name: "Directory 2" },
-        { dirId: "3", name: "Directory 3" },
-        { dirId: "4", name: "Directory 4" },
-      ],
-    },
+    props: { project: projects[0] },
+  };
+}
+
+export async function getStaticPaths() {
+  const paths = data.map((project) => ({
+    params: { id: project.id.toString() },
+  }));
+  console.log(paths);
+  return {
+    paths,
+    fallback: false, // can also be true or 'blocking'
   };
 }
