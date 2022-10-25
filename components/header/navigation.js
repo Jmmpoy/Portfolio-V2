acafb8078ae2b584c286e9c1de59fb8882c4f822
Jmmpoy <dunker35@hotmail.fr>
2022-10-25 16:07:43 +0200

    Nav + projectPage

--- components/header/navigation.js
+++ components/header/navigation.js
@@ -1,20 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 import Link from "next/link";
 import { fade } from "@/helpers/transitions";
 export default function Navigation({ items, activeIndex, setActiveIndex }) {
+  const [AnimationIsDone, SetAnimationIsDone] = useState(false);
+
   return (
     <motion.nav className="self-baseline flex  xsm:basis-1/2  md:flex items-center w-full  md:w-auto">
       <div className="flex">
         <Link href="/" class="relative">
-          <a className="uppercase text-sm self-center font-founders">
-            {" "}
-            Lucas Reteau{" "}
-          </a>
+          <motion.a
+            variants={fade}
+            initial="initial"
+            animate="enter"
+            exit="exit"
+            className="uppercase text-sm self-center font-founders cursor-pointer">
+            Home
+          </motion.a>
         </Link>
-        <div className="relative w-5">
+        <motion.div
+          variants={fade}
+          initial="initial"
+          animate="enter"
+          exit="exit"
+          className="relative w-5">
           <span className="line"></span>
-        </div>
+        </motion.div>
         <motion.ul
           variants={fade}
           initial="initial"
--- pages/projects/[id].js
+++ pages/projects/[id].js
@@ -5,40 +5,261 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 import Container from "../../components/container";
 import data from "../../api.js";
+import { motion } from "framer-motion";
+import { delayedFade, fade } from "@/helpers/transitions";
 
 export default function Project({ project }) {
-  console.log("project", project);
+  const Title = ({
+    animateDelay,
+    animateDuration,
+    exitDelay,
+    exitDuration,
+    classes,
+  }) => {
+    return (
+      <motion.p
+        initial={{ y: 100 }}
+        animate={{
+          y: 0,
+          transition: {
+            delay: animateDelay,
+            duration: animateDuration,
+            ease: "easeInOut",
+          },
+        }}
+        exit={{
+          y: 100,
+          transition: {
+            delay: exitDelay,
+            duration: exitDuration,
+            ease: "easeInOut",
+          },
+        }}
+        className="text-5xl xsm:text-6xl sm:text-7xl md:text-8xl font-neueRegular tracking-tight ">
+        {project.name}
+      </motion.p>
+    );
+  };
 
-  return (
-    <Container extraClasses="Content-Container relative  h-screen py-12 overflow-auto  md:overflow-hidden  ">
-      <div className="  grid  h-full mt-12  md:grid-cols-2 md:mt-32 ">
-        <div className="text-sm font-founders  w-5/5 sm:w-4/5 md:m-0 ">
-          <p className="text-5xl xsm:text-5xl sm:text-4xl md:text-4xl font-foundersLight tracking-tight hover:text-${project.color} ">
-            {project.name}
-          </p>
-          <p className="text-sm font-foundersLight mt-8 w-5/5 max-w-[450px]">
-            {project.description}
-          </p>
-          <p className="text-[12px] font-foundersLight mt-4 ">{project.role}</p>
-          <p className="text-[10px] font-foundersLight mt-[-12px]">
-            {project.year}
-          </p>
+  const ProjectInfos = ({ titleStyle, contentStyle }) => {
+    return (
+      <motion.ul className=" mt-9 md:mt-3  grid md:grid-cols-2  overflow-hidden">
+        <motion.li className="flex flex-col justify-evenly">
+          <div className="overflow-hidden">
+            <motion.p
+              initial={{ y: 40 }}
+              animate={{
+                y: 0,
+                transition: {
+                  delay: 1,
+                  duration: 1.5,
+                  ease: "easeInOut",
+                },
+              }}
+              exit={{
+                y: 100,
+                transition: {
+                  delay: 0.3,
+                  duration: 0.8,
+                  ease: "easeInOut",
+                },
+              }}
+              className={titleStyle}>
+              Role
+            </motion.p>
+          </div>
+          <div className="overflow-hidden">
+            <motion.p
+              initial={{ y: 40 }}
+              animate={{
+                y: 0,
+                transition: {
+                  delay: 1.5,
+                  duration: 1.5,
+                  ease: "easeInOut",
+                },
+              }}
+              exit={{
+                y: 100,
+                transition: {
+                  delay: 0.7,
+                  duration: 0.8,
+                  ease: "easeInOut",
+                },
+              }}
+              className={contentStyle}>
+              {project.role}
+            </motion.p>
+          </div>
+        </motion.li>
+        <motion.li className="flex flex-col justify-evenly">
+          <div className="overflow-hidden">
+            <motion.p
+              initial={{ y: 40 }}
+              animate={{
+                y: 0,
+                transition: {
+                  delay: 1,
+                  duration: 1.5,
+                  ease: "easeInOut",
+                },
+              }}
+              exit={{
+                y: 100,
+                transition: {
+                  delay: 0.3,
+                  duration: 0.8,
+                  ease: "easeInOut",
+                },
+              }}
+              className={titleStyle}>
+              Year
+            </motion.p>
+          </div>
+          <div className="overflow-hidden">
+            <motion.p
+              initial={{ y: 40 }}
+              animate={{
+                y: 0,
+                transition: {
+                  delay: 1.5,
+                  duration: 1.5,
+                  ease: "easeInOut",
+                },
+              }}
+              exit={{
+                y: 100,
+                transition: {
+                  delay: 0.7,
+                  duration: 0.8,
+                  ease: "easeInOut",
+                },
+              }}
+              className={contentStyle}>
+              {project.year}
+            </motion.p>
+          </div>
+        </motion.li>
+      </motion.ul>
+    );
+  };
+
+  const Description = () => {
+    return (
+      <motion.div
+        variants={fade}
+        initial="initial"
+        animate="enter"
+        exit="exit"
+        className="mt-9 md:mt-40">
+        {/* Description */}
+        <motion.div className="mb-8">
+          {project.description.map((item) => {
+            console.log(item);
+            const isGray = item.id === 3 ? "text-gray" : "text-black";
+            return (
+              <div className="overflow-hidden">
+                <motion.p
+                  initial={{ y: `${40 * item.id}` }}
+                  animate={{
+                    y: 0,
+                    transition: {
+                      delay: `${0.2 * item.id}`,
+                      duration: 1.5,
+                      ease: "easeInOut",
+                    },
+                  }}
+                  exit={{
+                    y: 100,
+                    transition: {
+                      delay: `${0.1 * item.id}`,
+                      duration: 0.8,
+                      ease: "easeInOut",
+                    },
+                  }}
+                  key={item.id}
+                  class="text-[3vw] sm:text-xl  md:text-2xl font-foundersLight">
+                  {item.text}
+                </motion.p>
+              </div>
+            );
+          })}
+        </motion.div>
+        <div className="flex ">
+          <a className="underline font-foundersLight" href={project.link}>
+            Visiter le site
+          </a>
+          <div className="flex flex-col justify-end">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 700 700"
+              fill="#000"
+              stroke="currentColor"
+              class="w-4 h-4">
+              <path d="m280 105v35h185.25l-285.13 285.13 24.746 24.746 285.13-285.13v185.25h35v-245z" />
+            </svg>
+          </div>
+        </div>
+      </motion.div>
+    );
+  };
+
+  const ProjectImages = ({ image, secondaryImage }) => {
+    console.log("image", image);
+
+    return (
+      <motion.div
+        variants={fade}
+        initial="initial"
+        animate="enter"
+        exit="exit"
+        className="image-container mt-52 mx-auto  py-20 px-12  flex flex-col  gap-y-32  justify-center bg-black sm: gap-x-20 md:flex-row md:p-24 ">
+        <div className="basis-full project-image-card ">
+          <Image
+            src={image}
+            blurDataURL={project.blurDataURL}
+            placeholder={``}
+            alt={project.name}
+            class=" min-w-[320px]  image basis-full"
+          />
         </div>
 
-        <div className="  mt-20 w-5/5  md:mt-0  md:min-w-[200px] ">
-          {project && (
+        <div className="basis-full project-image-card ">
+          {secondaryImage && (
             <Image
-              src={project.url}
+              src={secondaryImage}
               blurDataURL={project.blurDataURL}
               placeholder="blur"
               alt={project.name}
-              width={460}
-              height={258}
-              class=" min-w-[320px] w-full h-auto object-cover object-center aspect-auto"
+              class=" min-w-[320px]  image "
             />
           )}
         </div>
-      </div>
+      </motion.div>
+    );
+  };
+
+  return (
+    <Container extraClasses="Content-Container relative  h-screen py-12 overflow-auto   ">
+      <motion.div className="flex flex-col  overflow-hidden   mt-12 mb-12 md:grid md:grid-cols-2   md:mt-36">
+        <Title
+          animateDelay="0.2"
+          animateDuration="1.5"
+          exitDelay="0.3"
+          exitDuration="0.8"
+          classes="text-5xl xsm:text-6xl sm:text-7xl font-foundersLight tracking-tight"
+        />
+        <ProjectInfos
+          titleStyle="text-sm text-gray "
+          contentStyle="text-xl font-foundersLight "
+        />
+      </motion.div>
+
+      <Description />
+      <ProjectImages
+        image={project.primaryImage}
+        secondaryImage={project.secondaryImage}
+      />
     </Container>
   );
 }
