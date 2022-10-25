fa5b9b699d2ffa445081606882bb0936eead6beb
Jmmpoy <dunker35@hotmail.fr>
2022-10-25 16:05:45 +0200

    Hero

--- components/hero.js
+++ components/hero.js
@@ -1,30 +1,104 @@
-import React from "react";
-import { m } from "framer-motion";
+import React, { useState } from "react";
+import { motion } from "framer-motion";
 import Container from "@/components/container";
-import { fade } from "@/helpers/transitions";
+import { fade, stagger, slideInLeft } from "@/helpers/transitions";
 
 export default function Hero() {
+  const content = [
+    { id: 1, text: "Mpoy Jean-Marc" },
+    { id: 2, text: "— front end developer" },
+    { id: 3, text: "based in Paris" },
+    { id: 4, text: "*Curently working at Ucaya" },
+  ];
+  const container = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.1,
+        delayChildren: 0.2,
+      },
+    },
+    exit: {
+      opacity: 0,
+      transition: {
+        duration: 2,
+        staggerChildren: 0.9,
+        delayChildren: 0.2,
+      },
+    },
+  };
+
+  const [HeroAnimationIsDone, SetHeroAnimationIsDone] = useState(false);
+
   return (
     <Container extraClasses="Hero-Container relative ">
-      <m.main class=" h-[65vh] flex flex-col justify-center">
-        <m.div variants={fade} initial="initial" animate="enter" exit="exit">
-          <div class="">
-            <h1 class=" hero-font-size  mb-0 font-ibmLight  uppercase  hero-font-space-small xsm:text-3xl sm:mb-3   sm:text-5xl sm:hero-font-space">
-              Lucas reteau
-            </h1>
-            <div>
-              <h1 class=" hero-font-size  mb-0 font-ibmLight  uppercase  hero-font-space-small xsm:text-3xl  sm:mb-3  sm:text-5xl sm:hero-font-space  ">
-                front end developer
-              </h1>
-            </div>
-            <div>
-              <h1 class=" hero-font-size  mb-0 font-ibmLight text-gray  uppercase  hero-font-space-small xsm:text-3xl  sm:mb-3  sm:text-5xl sm:hero-font-space  ">
-                based in Nantes
-              </h1>
-            </div>
-          </div>
-        </m.div>
-      </m.main>
+      <main class=" h-[65vh] flex flex-col justify-center">
+        <motion.div>
+          <motion.ul
+            variants={container}
+            initial="hidden"
+            animate="show"
+            exit="exit">
+            {content.map((item) => {
+              const isGray = item.id === 3 ? "text-gray" : "text-black";
+              const isSecondLine = item.id === 2;
+              const currentStatus = item.id === 4;
+              return (
+                <div className="overflow-hidden">
+                  {currentStatus ? (
+                    <motion.p
+                      initial={{ y: `${70 * item.id}` }}
+                      animate={{
+                        y: 0,
+                        transition: {
+                          delay: 3,
+                          duration: 1,
+                          ease: "easeInOut",
+                        },
+                      }}
+                      exit={{
+                        y: 100,
+                        transition: {
+                          delay: `${0.1 * item.id}`,
+                          duration: 0.8,
+                          ease: "easeInOut",
+                        },
+                      }}
+                      key={item.id}
+                      className="text-[10px] font-foundersLight">
+                      *Curently working at Ucaya
+                    </motion.p>
+                  ) : (
+                    <motion.li
+                      initial={{ y: `${70 * item.id}` }}
+                      animate={{
+                        y: 0,
+                        transition: {
+                          delay: `${0.2 * item.id}`,
+                          duration: 1,
+                          ease: "easeInOut",
+                        },
+                      }}
+                      exit={{
+                        y: 100,
+                        transition: {
+                          delay: `${0.1 * item.id}`,
+                          duration: 0.8,
+                          ease: "easeInOut",
+                        },
+                      }}
+                      key={item.id}
+                      class={` ${isGray} hero-font-size  mb-0 font-neueLight uppercase   xsm:text-3xl sm:mb-3   sm:text-5xl`}>
+                      {item.text}
+                    </motion.li>
+                  )}
+                </div>
+              );
+            })}
+          </motion.ul>
+        </motion.div>
+      </main>
     </Container>
   );
 }
