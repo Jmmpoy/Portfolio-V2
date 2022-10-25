8e7612081407c13d1888ccd358875637397386ea
Jmmpoy <dunker35@hotmail.fr>
2022-10-25 16:06:09 +0200

    Content

--- components/content.js
+++ components/content.js
@@ -1,32 +1,62 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Container from "./container";
 import Link from "next/link";
 import Image from "next/image";
 import data from "../api.js";
+import { useAnimation, motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { delayedFade, fade } from "@/helpers/transitions";
 
 export default function content() {
-  const ROUTE_PROJECT_ID = "projects/[id]";
+  function FadeInWhenVisible({ children }) {
+    const controls = useAnimation();
+    const [ref, inView] = useInView({ margin: "70px" });
 
-  console.log("data", data.data);
-  // console.log("api", api);
+    useEffect(() => {
+      if (inView) {
+        controls.start("visible");
+      }
+    }, [controls, inView]);
+
+    return (
+      <motion.div
+        class={` project border-t-[0.2px] border-gray  h-24 md:h-32 flex justify-between items-center text-sm uppercase relative text-black hover:text-gray  transition duration-700`}
+        ref={ref}
+        animate={controls}
+        initial="hidden"
+        exit="exit"
+        transition={{ duration: 0.4 }}
+        variants={{
+          hidden: { opacity: 0, y: 20 },
+          visible: { opacity: 1, y: 0 },
+          exit: { opacity: 0, y: 20 },
+        }}>
+        {children}
+      </motion.div>
+    );
+  }
 
   return (
     <Container extraClasses="Content-Container   py-12   lg:py-32">
-      <p class="text-[16px] text-gray font-foundersLight ">Selected projects</p>
-
-      <div class="h-full mt-4">
+      <motion.p
+        variants={delayedFade}
+        initial="initial"
+        animate="enter"
+        exit="exit"
+        class="text-[16px] text-gray font-foundersLight ">
+        Selected projects
+      </motion.p>
+      <motion.div class="h-full mt-4">
         {data.map((project, index) => {
           console.log(project);
           return (
-            <div
-              key={`project-${project.id}`}
-              class={` project border-t-[0.2px] border-gray  h-24 md:h-32 flex justify-between items-center text-sm uppercase relative text-black hover:text-gray  transition duration-700`}>
+            <FadeInWhenVisible key={`project-${project.id}`}>
               <Link
                 href={`/projects/[id]`}
                 as={`/projects/${project.id}`}
                 passHref>
                 <a
-                  className={`  text-5xl xsm:text-6xl sm:text-7xl md:text-8xl font-foundersLight tracking-tight hover:text-${project.color}  `}>
+                  className={`  text-5xl xsm:text-6xl sm:text-7xl md:text-8xl font-foundersLight tracking-tight  `}>
                   {project.name}
                 </a>
               </Link>
@@ -45,10 +75,10 @@ export default function content() {
                   />
                 </svg>
               </div>
-            </div>
+            </FadeInWhenVisible>
           );
         })}
-      </div>
+      </motion.div>
     </Container>
   );
 }
