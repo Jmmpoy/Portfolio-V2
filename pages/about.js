5c5b4e31adf0a0bbe221acafa1e6b9760ab30477
Jmmpoy <dunker35@hotmail.fr>
2022-10-25 16:04:54 +0200

    About Page

--- pages/about.js
+++ pages/about.js
@@ -4,27 +4,151 @@ import Header from "@/components/header/header";
 import Footer from "@/components/footer";
 import Container from "@/components/container";
 import FancyLink from "@/components/fancyLink";
-import { fade } from "@/helpers/transitions";
+import { fade, delayedFade } from "@/helpers/transitions";
 import { LocomotiveScrollProvider } from "react-locomotive-scroll";
 import { LazyMotion, domAnimation, m } from "framer-motion";
 import { NextSeo } from "next-seo";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function About() {
-  const containerRef = useRef(null);
+  const presentation = [
+    {
+      id: 1,
+      text: "Mpoy Jean-Marc is a front-end",
+    },
+
+    {
+      id: 2,
+      text: "developer based in France, ",
+    },
+    {
+      id: 3,
+      text: "evolving his craft in the digital ",
+    },
+    {
+      id: 4,
+      text: "industry for more than 8 years.",
+    },
+  ];
+  const content = [
+    {
+      id: 0,
+      text: "Passionate about design he speciales in ",
+    },
+    {
+      id: 1,
+      text: "web experiences with a strong focus on ",
+    },
+    {
+      id: 2,
+      text: " animation and interaction.",
+    },
+  ];
+
+  const content2 = [
+    {
+      id: 3,
+      text: "As a front end developer, he had the",
+    },
+    {
+      id: 4,
+      text: "opportunity to collaborate with a broad",
+    },
+    {
+      id: 5,
+      text: "range of clients that includes Microsoft, ",
+    },
+    {
+      id: 6,
+      text: " Canal+ , Soundcharts or FilmoTv.",
+    },
+  ];
+
   return (
-    <Container extraClasses="Content-Container  h-screen py-12 overflow-auto  md:overflow-hidden  ">
-      <div class="grid  h-full  md:grid-cols-2  ">
-        <div class="text-sm font-founders col-start-2 flex flex-col justify-center w-4/5 mx-auto md:m-0 md:w-full ">
-          <p class="text-2xl font-foundersLight">
-            I’m a freelance Designer, Director based in Stockholm, Sweden.
-            Currently @ Nike Technology Innovation. My passion lies in art
-            direction, branding, typography, Interface/ux & system design,
-            motion and technology. Previously, I worked at Ri, AKQA, RG/A,
-            Google Creative Lab, Unit9, MediaMonks, Wieden + Kennedy and Waste
-            Creative.
-          </p>
-        </div>
-      </div>
+    <Container extraClasses="Content-Container  h-screen py-12 overflow-auto  md:overflow-hidden">
+      <motion.div class="grid  h-full grid-cols-1   md:grid-cols-2 md:bg-blue-500 ">
+        <motion.div class="text-sm font-founders  flex flex-col justify-center w-full md:col-start-2  md:m-0 ">
+          <motion.div
+            variants={delayedFade}
+            initial="initial"
+            animate="enter"
+            exit="exit">
+            {presentation.map((item, index) => {
+              return (
+                <p class="text-2xl sm:text-[1.6rem] font-neueRegular list-none max-w-lg mb-0">
+                  {item.text}
+                </p>
+              );
+            })}
+          </motion.div>
+          <div>
+            <div className="mt-16 ">
+              {content.map((item, index) => {
+                return (
+                  <motion.div className="overflow-hidden " key={index}>
+                    <motion.li
+                      initial={{ opacity: 0, y: `${40 * item.id}` }}
+                      animate={{
+                        y: 0,
+                        opacity: 1,
+                        transition: {
+                          delay: `${0.1 * item.id}`,
+                          duration: 1,
+                          ease: "easeInOut",
+                        },
+                      }}
+                      exit={{
+                        y: 50,
+                        opacity: 0,
+                        transition: {
+                          delay: `${0.1 * item.id}`,
+                          duration: 0.4,
+                          ease: "easeInOut",
+                        },
+                      }}
+                      key={item.id}
+                      class="text-[3.7vw] leading-[25px] xsm:text-[18px]  font-founders list-none sm:leading-7 sm:text-[18px]">
+                      {item.text}
+                    </motion.li>
+                  </motion.div>
+                );
+              })}
+            </div>
+            <div className="mt-4 ">
+              {content2.map((item, index) => {
+                return (
+                  <motion.div className="overflow-hidden " key={index}>
+                    <motion.li
+                      initial={{ opacity: 0, y: `${40 * item.id}` }}
+                      animate={{
+                        y: 0,
+                        opacity: 1,
+                        transition: {
+                          delay: `${0.1 * item.id}`,
+                          duration: 1,
+                          ease: "easeInOut",
+                        },
+                      }}
+                      exit={{
+                        y: 50,
+                        opacity: 0,
+                        transition: {
+                          delay: `${0.1 * item.id}`,
+                          duration: 0.4,
+                          ease: "easeInOut",
+                        },
+                      }}
+                      key={item.id}
+                      class="text-[3.7vw] leading-[25px] xsm:text-[18px]  font-founders list-none sm:leading-7 sm:text-[18px]">
+                      {item.text}
+                    </motion.li>
+                  </motion.div>
+                );
+              })}
+            </div>
+          </div>
+        </motion.div>
+      </motion.div>
     </Container>
   );
 }
