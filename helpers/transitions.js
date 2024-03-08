d3bab14f6fb330ca84656af0bec03df38758f0c7
Jean-Marc <jeanmarc.mpoy@nalo.fr>
2024-03-08 23:23:13 +0100

    fixed stransition

--- helpers/transitions.js
+++ helpers/transitions.js
@@ -14,11 +14,18 @@ export const delayedFade = {
   initial: { opacity: 0 },
   enter: {
     opacity: 1,
-    transition: { duration: 1.8, ease: [0.83, 0, 0.17, 1], delay: 1 },
+    transition: { 
+      duration: 1, 
+      ease: [0.7, 0, 0.3, 1], 
+      delay: .5 
+    },
   },
   exit: {
     opacity: 0,
-    transition: { duration: 1.8, ease: [0.83, 0, 0.17, 1] },
+    transition: { 
+      duration: 1.5,
+      ease: [0.7, 0, 0.3, 1] 
+    },
   },
 };
 
