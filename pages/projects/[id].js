96a9c4b671605af8d6e29fdba23a08ed3ff29624
Jean-Marc <jeanmarc.mpoy@nalo.fr>
2024-03-08 23:16:32 +0100

    project page

--- pages/projects/[id].js
+++ pages/projects/[id].js
@@ -17,35 +17,45 @@ export default function Project({ project }) {
     classes,
   }) => {
     return (
-      <span className="flex flex-col  overflow-hidden">
-        <motion.p
-          initial={{ y: 100 }}
-          animate={{
-            y: 0,
-            transition: {
-              delay: animateDelay,
-              duration: animateDuration,
-              ease: "easeInOut",
-            },
-          }}
-          exit={{
-            y: 100,
-            transition: {
-              delay: exitDelay,
-              duration: exitDuration,
-              ease: "easeInOut",
-            },
-          }}
-          className={classes}>
-          {project.name}
-        </motion.p>
-      </span>
+      <div className="flex flex-col-reverse sm:flex-row">
+        <span className=" w-full">
+          <p className="font-sohneBuch text-[10px] mb-4 sm:mb-0 sm:text-xs uppercase ">
+            {project.tags}
+          </p>
+          <motion.a
+            initial={{ y: 100 }}
+            animate={{
+              y: 0,
+              transition: {
+                delay: animateDelay,
+                duration: animateDuration,
+                ease: "easeInOut",
+              },
+            }}
+            exit={{
+              y: 100,
+              transition: {
+                delay: exitDelay,
+                duration: exitDuration,
+                ease: "easeInOut",
+              },
+            }}
+            className={`${classes} relative`}
+            href={project.link}
+          >
+            {project.name}
+          </motion.a>
+        </span>
+        <div className="font-sohneBuch text-xs uppercase text-left sm:text-right w-full text-[#373737]">
+          {project.year}
+        </div>
+      </div>
     );
   };
 
   const ProjectInfos = ({ titleStyle, contentStyle }) => {
     return (
-      <motion.ul className=" mt-9 md:mt-3  grid md:grid-cols-2  overflow-hidden">
+      <motion.ul className=" mt-9 md:mt-3  overflow-hidden">
         <motion.li className="flex flex-col justify-evenly">
           <div className="overflow-hidden">
             <motion.p
@@ -66,7 +76,8 @@ export default function Project({ project }) {
                   ease: "easeInOut",
                 },
               }}
-              className={titleStyle}>
+              className={titleStyle}
+            >
               Rôle
             </motion.p>
           </div>
@@ -89,7 +100,8 @@ export default function Project({ project }) {
                   ease: "easeInOut",
                 },
               }}
-              className={contentStyle}>
+              className={contentStyle}
+            >
               {project.role}
             </motion.p>
           </div>
@@ -114,7 +126,8 @@ export default function Project({ project }) {
                   ease: "easeInOut",
                 },
               }}
-              className={titleStyle}>
+              className={titleStyle}
+            >
               Année
             </motion.p>
           </div>
@@ -137,7 +150,8 @@ export default function Project({ project }) {
                   ease: "easeInOut",
                 },
               }}
-              className={contentStyle}>
+              className={contentStyle}
+            >
               {project.year}
             </motion.p>
           </div>
@@ -153,8 +167,9 @@ export default function Project({ project }) {
         initial="initial"
         animate="enter"
         exit="exit"
-        className="mt-9 md:mt-40">
-        <motion.div className="mb-8">
+        className="mt-8 text-left sm:text-right"
+      >
+        <motion.div className="mb-8 ">
           {project.description.map((item, index) => {
             const isGray = item.id === 3 ? "text-gray" : "text-black";
             return (
@@ -178,87 +193,92 @@ export default function Project({ project }) {
                     },
                   }}
                   key={item.id}
-                  class="text-[3vw] sm:text-xl  md:text-2xl font-neueRegular">
+                  class="text-sm  font-sohneBuch text-[#373737]"
+                >
                   {item.text}
                 </motion.p>
               </div>
             );
           })}
         </motion.div>
-        <div className="flex ">
-          <a className="underline font-neueRegular" href={project.link}>
-            Visiter le site
-          </a>
-          <div className="flex flex-col justify-end">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 700 700"
-              fill="#000"
-              stroke="currentColor"
-              class="w-4 h-4">
-              <path d="m280 105v35h185.25l-285.13 285.13 24.746 24.746 285.13-285.13v185.25h35v-245z" />
-            </svg>
-          </div>
-        </div>
       </motion.div>
     );
   };
 
-  const ProjectImages = ({ image, secondaryImage }) => {
+  const ProjectImages = ({ image, secondaryImage, thirdImage }) => {
     return (
       <motion.div
         variants={fade}
         initial="initial"
         animate="enter"
         exit="exit"
-        className="image-container min-h-[420px]  mx-auto mt-24 md:mt-48 py-16 px-12  flex flex-col  gap-y-24  justify-center  sm: gap-x-20 md:flex-row md:p-24 ">
-        <motion.div className="basis-full project-image-card max-w-[700px] ">
-          <Image
-            src={image}
-            blurDataURL={image.blurDataURL}
-            placeholder="blur"
-            alt={project.name}
-            class=" min-w-[320px]  image basis-full"
-          />
-        </motion.div>
-
-        <motion.div className="basis-full project-image-card max-w-[700px] ">
-          {secondaryImage && (
+        className="image-container min-h-[420px]  mx-auto  py-16 px-12  flex flex-col  gap-y-24  justify-center items-center  md:p-24 "
+      >
+        {image && (
+          <motion.div className="basis-full project-image-card  ">
             <Image
-              src={secondaryImage}
-              blurDataURL={secondaryImage.blurDataURL}
+              src={image}
+              blurDataURL={image.blurDataURL}
               placeholder="blur"
               alt={project.name}
-              class=" min-w-[320px]  image "
+              class=" min-w-[320px]  image basis-full"
             />
-          )}
-        </motion.div>
+          </motion.div>
+        )}
+
+        {secondaryImage && (
+          <motion.div className="basis-full project-image-card  ">
+            {secondaryImage && (
+              <Image
+                src={secondaryImage}
+                blurDataURL={secondaryImage.blurDataURL}
+                placeholder="blur"
+                alt={project.name}
+                class=" min-w-[320px]  image "
+              />
+            )}
+          </motion.div>
+        )}
+
+        {thirdImage && (
+          <motion.div className="basis-full project-image-card  ">
+            {thirdImage && (
+              <Image
+                src={thirdImage}
+                blurDataURL={thirdImage.blurDataURL}
+                placeholder="blur"
+                alt={project.name}
+                class=" min-w-[320px]  image "
+              />
+            )}
+          </motion.div>
+        )}
       </motion.div>
     );
   };
 
-  return (
-    <Container extraClasses="Content-Container relative  h-screen py-12">
-      <motion.div className="flex flex-col  overflow-hidden   mt-8 mb-12 md:grid md:grid-cols-2   md:mt-24">
-        <Title
-          animateDelay="0.2"
-          animateDuration="1.5"
-          exitDelay=".7"
-          exitDuration="1"
-          classes="text-5xl xsm:text-6xl sm:text-7xl font-neueRegular tracking-tight"
-        />
-        <ProjectInfos
-          titleStyle="text-base neueRegular  "
-          contentStyle="text-xl font-neueRegular "
-        />
-      </motion.div>
+  const projectTitle = [{ id: project.id, text: project.name }];
 
-      <Description />
+  return (
+    <>
+      <Container extraClasses="Content-Container relative pt-8 md:py-12">
+        <motion.div className="flex flex-col my-16 mb-12 overflow-hidden  md:mt-20">
+          <Title classes="hero-font-size text-5xl mt-8 md:mt-0  mb-0 uppercase font-sohneKraftig tracking-tighter  xsm:text-7xl     md:text-9xl  lg:text-9xl transition duration-300 ease-in-out hover:text-[#505050]" />
+          <Description />
+        </motion.div>
+      </Container>
       <ProjectImages
         image={project.primaryImage}
         secondaryImage={project.secondaryImage}
+        thirdImage={project.thirdImage}
       />
-    </Container>
+
+      {/* <Description /> */}
+      {/* <ProjectInfos
+        titleStyle="text-base neueRegular  "
+        contentStyle="text-xl font-neueRegular "
+      /> */}
+    </>
   );
 }
 
