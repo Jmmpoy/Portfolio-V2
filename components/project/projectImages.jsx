a788c03e3b9bdc97e5436001cd596e124c979a95
Jean-Marc <jeanmarc.mpoy@nalo.fr>
2024-04-01 01:16:36 +0200

    components

---
+++ components/project/projectImages.jsx
@@ -0,0 +1,82 @@
+import {motion} from 'framer-motion'
+import Image from 'next/image';
+import SmoothScrolling from '@/components/smoothScrolling';
+
+export  const ProjectImages = ({
+    image,
+    secondaryImage,
+    thirdImage,
+    fourthImage,
+    name
+  }) => {
+    return (
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{
+          opacity: 1,
+          transition: { delay: 0.6, duration: 0.7, ease: "easeInOut" },
+        }}
+        exit={{
+          opacity: 0,
+          transition: { delay: 0.6, duration: 0.7, ease: "easeInOut" },
+        }}
+        className="image-container min-h-[420px] sepia-[10%]  mx-auto flex flex-col  justify-center items-center gap-y-8   "
+      >
+        {image && (
+          <motion.div className="basis-full project-image-card   ">
+            <Image
+              src={image}
+              blurDataURL={image.blurDataURL}
+              placeholder="blur"
+              alt={name}
+              class=" min-w-[320px]  image basis-full"
+            />
+          </motion.div>
+        )}
+
+        {secondaryImage && (
+          <motion.div className="basis-full project-image-card  ">
+            {secondaryImage && (
+              <Image
+                src={secondaryImage}
+                blurDataURL={secondaryImage.blurDataURL}
+                placeholder="blur"
+                alt={name}
+                class=" min-w-[320px]  image "
+              />
+            )}
+          </motion.div>
+        )}
+
+        {thirdImage && (
+          <motion.div className="basis-full project-image-card  ">
+            {thirdImage && (
+              <Image
+                src={thirdImage}
+                blurDataURL={thirdImage.blurDataURL}
+                placeholder="blur"
+                alt={name}
+                class=" min-w-[320px]  image "
+              />
+            )}
+          </motion.div>
+        )}
+
+        {fourthImage && (
+          <motion.div className="basis-full project-image-card  ">
+            {fourthImage && (
+              <Image
+                src={fourthImage}
+                blurDataURL={fourthImage.blurDataURL}
+                placeholder="blur"
+                alt={name}
+                class=" min-w-[320px]  image "
+              />
+            )}
+          </motion.div>
+        )}
+      </motion.div>
+    );
+  };
+
+  export default ProjectImages
---
+++ components/project/title.jsx
@@ -0,0 +1,27 @@
+import {motion} from "framer-motion"
+
+export   const Title = ({classes, name, link }) => {
+    return (
+      <motion.div className="overflow-hidden">
+        <motion.a
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            transition: { delay: 0.7, duration: 0.7, ease: "easeInOut" },
+          }}
+          exit={{
+            opacity: 0,
+            transition: { delay: 0.6, duration: 0.7, ease: "easeInOut" },
+          }}
+          className={`${classes} relative`}
+          href={link}
+        >
+          {name}
+        </motion.a>
+      </motion.div>
+    );
+  };
+
+  export default Title;
+
+
