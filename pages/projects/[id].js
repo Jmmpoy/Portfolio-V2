6d936a15d655c1e04d539c6f3ca33f37f171fb21
Jean-Marc <jeanmarc.mpoy@nalo.fr>
2024-04-01 01:18:08 +0200

    fix project page

--- pages/projects/[id].js
+++ pages/projects/[id].js
@@ -1,231 +1,59 @@
 import React from "react";
-import Link from "next/link";
-
-import Image from "next/image";
-import { useRouter } from "next/router";
 import Container from "../../components/container";
 import data from "../../api.js";
-import { motion } from "framer-motion";
-import { delayedFade, fade } from "@/helpers/transitions";
-import { useEffect, useState } from "react";
+import Dates from "@/components/project/dates";
+import Tags from "@/components/project/tags";
+import Title from "@/components/project/title";
+import Description from "@/components/project/desccription";
+import Divider from "@/components/divider";
+import ProjectImages from "@/components/project/projectImages";
 
 export default function Project({ project }) {
-  const Title = ({
-    animateDelay,
-    animateDuration,
-    exitDelay,
-    exitDuration,
-    classes,
-  }) => {
-    return (
-      <motion.div className="overflow-hidden">
-        <motion.a
-          initial={{ opacity: 0 }}
-          animate={{
-            opacity: 1,
-            transition: { delay: 0.7, duration: 0.7, ease: "easeInOut" },
-          }}
-          exit={{
-            opacity: 0,
-            transition: { delay: 0.6, duration: 0.7, ease: "easeInOut" },
-          }}
-          className={`${classes} relative`}
-          href={project.link}
-        >
-          {project.name}
-        </motion.a>
-      </motion.div>
-    );
-  };
-
-  const Description = () => {
-    return (
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{
-          opacity: 1,
-          transition: { delay: 0.9, duration: 0.7, ease: "easeInOut" },
-        }}
-        exit={{
-          opacity: 0,
-          transition: { delay: 0.6, duration: 0.7, ease: "easeInOut" },
-        }}
-        className="Description mt-8"
-      >
-        {project.description.map((item, index) => {
-          const isGray = item.id === 3 ? "text-gray" : "text-black";
-          return (
-            <div key={index} className="overflow-hidden">
-              <motion.p
-                key={item.id}
-                class="text-xs  font-sohneBuch text-[#373737]"
-              >
-                {item.text}
-              </motion.p>
-            </div>
-          );
-        })}
-      </motion.div>
-    );
-  };
-
-  const ProjectImages = ({
-    image,
+  const {
+    year,
+    tags,
+    name,
+    link,
+    description,
+    primaryImage,
     secondaryImage,
     thirdImage,
     fourthImage,
-  }) => {
-    return (
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{
-          opacity: 1,
-          transition: { delay: 0.6, duration: 0.7, ease: "easeInOut" },
-        }}
-        exit={{
-          opacity: 0,
-          transition: { delay: 0.6, duration: 0.7, ease: "easeInOut" },
-        }}
-        className="image-container min-h-[420px] sepia-[10%]  mx-auto flex flex-col  justify-center items-center gap-y-8   "
-      >
-        {image && (
-          <motion.div className="basis-full project-image-card   ">
-            <Image
-              src={image}
-              blurDataURL={image.blurDataURL}
-              placeholder="blur"
-              alt={project.name}
-              class=" min-w-[320px]  image basis-full"
-            />
-          </motion.div>
-        )}
+  } = project;
 
-        {secondaryImage && (
-          <motion.div className="basis-full project-image-card  ">
-            {secondaryImage && (
-              <Image
-                src={secondaryImage}
-                blurDataURL={secondaryImage.blurDataURL}
-                placeholder="blur"
-                alt={project.name}
-                class=" min-w-[320px]  image "
-              />
-            )}
-          </motion.div>
-        )}
 
-        {thirdImage && (
-          <motion.div className="basis-full project-image-card  ">
-            {thirdImage && (
-              <Image
-                src={thirdImage}
-                blurDataURL={thirdImage.blurDataURL}
-                placeholder="blur"
-                alt={project.name}
-                class=" min-w-[320px]  image "
-              />
-            )}
-          </motion.div>
-        )}
-
-        {fourthImage && (
-          <motion.div className="basis-full project-image-card  ">
-            {fourthImage && (
-              <Image
-                src={fourthImage}
-                blurDataURL={fourthImage.blurDataURL}
-                placeholder="blur"
-                alt={project.name}
-                class=" min-w-[320px]  image "
-              />
-            )}
-          </motion.div>
-        )}
-      </motion.div>
-    );
-  };
-
-  const Dates = () => {
-    return (
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{
-          opacity: 1,
-          transition: { delay: 1.1, duration: 0.7, ease: "easeInOut" },
-        }}
-        exit={{
-          opacity: 0,
-          transition: { delay: 0.6, duration: 0.7, ease: "easeInOut" },
-        }}
-        className="Dates font-sohneBuch text-xs uppercase w-full text-[#373737] "
-      >
-        {project.year}
-      </motion.div>
-    );
-  };
-
-  const Tags = () => {
-    return (
-      <motion.p
-        initial={{ opacity: 0 }}
-        animate={{
-          opacity: 1,
-          transition: { delay: 1.3, duration: 0.7, ease: "easeInOut" },
-        }}
-        exit={{
-          opacity: 0,
-          transition: { delay: 0.8, duration: 0.7, ease: "easeInOut" },
-        }}
-        className=" Tags font-sohneBuch text-xs mb-4 sm:mb-0 sm:text-xs  "
-      >
-        {project.tags}
-      </motion.p>
-    );
-  };
-
-  const Divider = () => {
-    return (
-      <motion.hr
-        initial={{ width: 0 }}
-        animate={{
-          width: "100%",
-          transition: { duration: 1.3, ease: "easeInOut" },
-        }}
-        exit={{
-          width: 0,
-          transition: { duration: 1.3, ease: "easeInOut" },
-        }}
-        className="border-b-1 border-dashed border-black opacity-20 my-2"
-      />
-    );
-  };
 
   return (
     <>
-      <div className="Content-Container overflow-scroll sm:overflow-hidden relative custom-height-md">
+      {project && <div className="Content-Container overflow-scroll sm:overflow-hidden relative custom-height-md">
         <div className="flex flex-col md:flex-row h-full">
           <Container extraClasses="w-full flex flex-col mt-12 py-12 md:w-1/3">
-            <Title classes="hero-font-size text-4xl s=:text-5xl  mt-8 md:mt-0 mb-0 uppercase font-sohneKraftig tracking-tighter  transition duration-300 ease-in-out hover:text-[#505050]" />
+            <Title
+              link={link}
+              name={name}
+              classes="hero-font-size text-4xl sm=:text-4xl md:text-4xl  mt-8 md:mt-0 mb-0 uppercase font-sohneHalbfett tracking-tighter "
+            />
             <Divider />
             <div className="flex flex-col grow justify-end">
-              <Description />
+              <Description description={description} />
               <Divider />
-              <Dates />
+              <Dates year={year} />
               <Divider />
-              <Tags />
+              <Tags tags={tags} />
             </div>
           </Container>
 
           <div className="w-full md:mt-12 px-6 py-12 md:px-8 md:w-2/3 overflow-auto">
             <ProjectImages
-              image={project.primaryImage}
-              secondaryImage={project.secondaryImage}
-              thirdImage={project.thirdImage}
-              fourthImage={project.fourthImage}
+            name={name}
+              image={primaryImage}
+              secondaryImage={secondaryImage}
+              thirdImage={thirdImage}
+              fourthImage={fourthImage}
             />
           </div>
         </div>
-      </div>
+      </div>}
     </>
   );
 }
